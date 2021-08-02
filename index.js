0b6c9aa6c3059873e002abe943c4b9978465ad26
Milo <50248166+Milo123459@users.noreply.github.com>
2021-08-02 15:02:15 +0100

    fix

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const fs = require('fs')
+const path = require('path');
 const getDirectories = source =>
   fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
 const uploadImage = imageName => {
-  const results = getDirectories('../Roblox/Versions')
+  const results = getDirectories(path.join(path.normalize(require('os').homedir()), 'AppData', 'Local', 'Roblox', 'Versions'))
   console.log(results)
   results.forEach(folder => {
     //console.log(folder.toString().substring(0,9))
-    fs.readdirSync('../Roblox/Versions/' + folder.toString()).forEach(file => {
+    fs.readdirSync(path.join(path.normalize(require('os').homedir()), 'AppData', 'Local', 'Roblox', 'Versions', folder.toString())).forEach(file => {
       if (file.toString() === 'RobloxStudioBeta.exe') {
         console.log('Found studio folder, updating..')
         console.log('Current studio folder:', folder.toString())
-        const makedr =
-          '../Roblox/Versions/' +
-          folder.toString() +
-          '/content/textures/ClassImages.png'
+        const makedr = path.join(path.normalize(require('os').homedir()), 'AppData', 'Local', 'Roblox', 'Versions', folder.toString(), '/content/textures/ClassImages.png')
         fs.copyFile(`./${imageName}.png`, makedr, err => {
           if (err) throw err
           console.log('File was updated in', makedr)
